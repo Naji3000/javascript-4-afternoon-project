317c30d1fe90c8b6253e060e66abe55c74f07276
Naji Ali <SupremeBinxx@MacBook-Pro.attlocal.net>
2019-08-18 21:38:06 -0500

    Finished

--- classes.js
+++ classes.js
@@ -30,8 +30,20 @@
 */
 
 //Code Here
-
-
+class Employee {
+  constructor(first_name, last_name, email, age){
+    this.first_name = first_name
+    this.last_name = last_name
+    this.email = email
+    this.age = age
+  }
+  makeWidget(){
+    return this.first_name + " " + this.last_name + ' Widget'
+  }
+  // Employee.prototype.makeWidget = function(){
+  //   return `${this.first_name} + " " + ${this.last_name} + " " + widget`
+  // }
+}
 ////////// PROBLEM 2 //////////
 
 /*
@@ -48,8 +60,18 @@
 */
 
 //Code Here
-
-
+class Manager extends Employee {
+  constructor(first_name, last_name, email, age, reports){
+    super(first_name, last_name, email, age)
+    this.reports = []
+  }
+  hire(employee){
+    this.reports.push(employee)
+  }
+  fire(index){
+    this.reports.splice(index,1)
+  }
+}
 ////////// PROBLEM 3 //////////
 
 /*
@@ -72,7 +94,56 @@
 */
 
 //Code Here
-
+class ProgressiveManager extends Manager {
+  constructor(first_name, last_name, email, age, reports, title, bonus){
+  super(first_name, last_name, email, age, reports)
+  this.title = 'Not a manager';
+  this.bonus = 0;
+}
+hire () {
+  super.hire();
+  if (this.reports.length >= 101) {
+    this.title = "Bestest Manager";
+  } 
+  else if (this.reports.length >= 51) {
+    this.title = "Manager Plus";
+  } 
+  else if (this.reports.length >= 11) {
+    this.title = "Manager";
+  } 
+  else if (this.reports.length >= 4) {
+    this.title = "Mostly Manager";
+  }
+  else if (this.reports.length >= 1) {
+    this.title = "Barely Manager";
+  } 
+  else if (this.reports.length === 0) {
+    this.title = "Not a manager";
+  }
+}
+fire () {
+  super.fire() 
+  if (this.reports.length >= 101) {
+    this.title = "Bestest Manager";
+  } 
+  else if (this.reports >= 51) {
+    this.title = "Manager Plus";
+  } 
+  else if (this.reports >= 11) {
+    this.title = "Manager";
+  } 
+  else if (this.reports >= 4) {
+    this.title = "Mostly Manager";
+  }
+  else if (this.reports >= 1) {
+    this.title = "Barely Manager";
+  } 
+  else if (this.reports === 0) {
+    this.title = "Not a Manager";
+}
+this.bonus += 100;
+}
+}
 
 
 ////////// PROBLEM 4 - Black Diamond //////////
@@ -99,5 +170,9 @@
 */
 
 //Code Here
-
+// class Machines = 
+// this.widgets_made_count - default 0
+// this.wear_and_tear_count - default 0
+// - needs_reboot - default false
+// if(this.wear_and_tear_count% === 0 )
 
